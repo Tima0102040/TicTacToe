a58d5164eb0f1b6035c17d081f3da2e776fcb2ae
Zalevskyi <maxzalevskyi@gmail.com>
2023-02-13 20:44:10 +0200

    Check diagonals

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ def check_win (mas, sign) :
         if row.count (sign) == 3:
             return sign
     for col in range (3):
-        if mas [0][col]==sign and mas [1][col==sign and mas [2][col]== sign :
+        if mas [0][col]==sign and mas [1][col]==sign and mas [2][col]== sign :
             return sign
+    if mas[0][0] == sign and mas[1][1] == sign and mas[2][2] == sign:
+        return sign
+    if mas[0][2] == sign and mas[1][1] == sign and mas[2][0] == sign:
+        return sign
+    return False
 
 
 pygame.init()
