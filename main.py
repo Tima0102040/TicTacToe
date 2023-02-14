7b2d230dba31f559dfd3b38c21037b39019f832e
Tima0102040 <voznesenskiytim@gmail.com>
2023-02-14 11:58:45 +0200

    Refactoring

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import pygame
 import sys
 
-def check_win (mas, sign) :
+def check_win (arr, sign) :
     zeroes = 0
-    for row in mas:
+    for row in arr:
         zeroes+= row.count(0)
         if row.count (sign) == 3:
             return sign
     for col in range (3):
-        if mas [0][col]==sign and mas [1][col]==sign and mas [2][col]== sign :
+        if arr [0][col]==sign and arr [1][col]==sign and arr [2][col]== sign :
             return sign
-    if mas[0][0] == sign and mas[1][1] == sign and mas[2][2] == sign:
+    if arr[0][0] == sign and arr[1][1] == sign and arr[2][2] == sign:
         return sign
-    if mas[0][2] == sign and mas[1][1] == sign and mas[2][0] == sign:
+    if arr[0][2] == sign and arr[1][1] == sign and arr[2][0] == sign:
         return sign
     if zeroes==0:
         return 'Draw'
